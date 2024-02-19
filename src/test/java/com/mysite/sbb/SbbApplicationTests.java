8b21f4863ca1b297691fadb2b51c83c08ecb1efd
leemirae <416homin@daum.net>
2024-02-19 20:49:13 +0900

    feat : Junit으로 테스트
    - 데이터를 조작하는 리포지토리가 정상적으로 작동하는지테스트한다.
    Close #1

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,31 @@
 package com.mysite.sbb;
 
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+
 
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionRepository questionRepository;
 
+	
+	@Test
+	void testJpa() {
+		 Question q = this.questionRepository.findBySubject("레포지토리 테스트");
+	        assertEquals(1, q.getId());
+	}
 }
