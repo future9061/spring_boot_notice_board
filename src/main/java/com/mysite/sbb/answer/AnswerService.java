5218544d0f70a91e60b5b63c8de8f974d3ee7061
leemirae <416homin@daum.net>
2024-02-20 15:51:25 +0900

    feat : 답변 기능 만들기
    - 질문에 답변을 작성할 수 있다.
    Close #7

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+
+    //답변을 생성하기 위해 create 메서드를 추가
+    public void create(Question question, String content) {
+        //Answer 객체 생성
+    	Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
+
+
